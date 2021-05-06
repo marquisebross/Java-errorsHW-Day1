ecb034ea5196fe35b395202e0a5911e7045691e6
MARQUISE ROSS <marquiseb_ross@yahoo.com>
2021-05-06 15:07:48 -0500

    first commit

---
+++ Java-Errors-HW/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Aye Aye!");
+        System.out.print("yes");
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Pick a color");
+        String color = userInput.nextLine;
+        System.out.println(Pick a style);
+        String style = user.nextLine();
+        System.out.println("add a name");
+        String name = userInput.nextLine();
+
+        System.out.printf("\n I want to paint my car %s.", color);
+        System.out.printf("\n My faorite color is %s.");
+
+
+
+
+
+    }
+}
